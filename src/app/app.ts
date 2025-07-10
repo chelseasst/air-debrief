ae45ad181dc79f4a980f2ba8b99d27ead0238632
Chelsea <chelsea.todorova@gmail.com>
2025-07-10 20:08:40 +0300

    Changed button hover effect

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,19 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Transrcipt } from './transrcipt';
-import { Home } from './home/home';
-import { Loading } from './loading/loading';
-import { Summary } from './summary/summary';
-import { Error } from './error/error';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterModule, Home, Loading, Summary, Error],
+  imports: [CommonModule, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.sass'
 })
 export class App {
   protected title = 'air-debrief';
-
-  constructor(public viewService: Transrcipt) { }
 }
